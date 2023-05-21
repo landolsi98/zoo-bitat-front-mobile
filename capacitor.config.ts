6e85d3ea54a7aefba82ad56ff85439e1c4bef9cb
landolsi98 <landolsi.jawher@yahoo.com>
2023-05-21 20:09:39 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ZooBitat_Front_Mobile',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
